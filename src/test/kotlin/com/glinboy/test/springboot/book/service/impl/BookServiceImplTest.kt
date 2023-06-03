a660df9001fe224137579d3aee52b78812719ef0
GLinBoy <hojjatabedie@gmail.com>
2023-06-03 15:13:54 +0200

    📦 NEW: Added test class for BookServiceImpl
    #58

---
+++ src/test/kotlin/com/glinboy/test/springboot/book/service/impl/BookServiceImplTest.kt
@@ -0,0 +1,22 @@
+package com.glinboy.test.springboot.book.service.impl
+
+import org.junit.jupiter.api.Test
+
+class BookServiceImplTest {
+
+    @Test
+    fun getBooks() {
+    }
+
+    @Test
+    fun getBook() {
+    }
+
+    @Test
+    fun saveBook() {
+    }
+
+    @Test
+    fun deleteBook() {
+    }
+}
