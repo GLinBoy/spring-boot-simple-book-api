cba9f1a260d443d4a34c8fbef9a0672bb7cb2e38
GLinBoy <hojjatabedie@gmail.com>
2023-06-03 15:14:59 +0200

    📦 NEW: Added test class for BookController
    #59

---
+++ src/test/kotlin/com/glinboy/test/springboot/book/web/api/BookResourceTest.kt
@@ -0,0 +1,30 @@
+package com.glinboy.test.springboot.book.web.api
+
+import org.junit.jupiter.api.Test
+
+class BookResourceTest {
+
+    @Test
+    fun getBooks() {
+    }
+
+    @Test
+    fun getBook() {
+    }
+
+    @Test
+    fun addBook() {
+    }
+
+    @Test
+    fun deleteBook() {
+    }
+
+    @Test
+    fun updateBook() {
+    }
+
+    @Test
+    fun getBookService() {
+    }
+}
