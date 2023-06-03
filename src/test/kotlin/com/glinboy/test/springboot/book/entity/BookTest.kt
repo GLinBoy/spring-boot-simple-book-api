b2ba1e0dd7bc5fcb96a23aad6de63eca72553da6
GLinBoy <hojjatabedie@gmail.com>
2023-06-03 15:03:56 +0200

    👌 IMPROVE: Added test for Book
    #56

--- src/test/kotlin/com/glinboy/test/springboot/book/entity/BookTest.kt
+++ src/test/kotlin/com/glinboy/test/springboot/book/entity/BookTest.kt
@@ -3,4 +3,18 @@ package com.glinboy.test.springboot.book.entity
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-internal class BookTest {}
+internal class BookTest {
+    companion object {
+        const val ID = 10001L
+        const val TITLE = "Test Title"
+        const val AUTHOR = "Test Author"
+        const val ISBN = "978-3-16-148410-0"
+    }
+
+    @Test
+    fun verifyEquals() {
+        var book1 = Book(ID, TITLE, AUTHOR, ISBN)
+        var book2 = book1.copy(ID, TITLE, AUTHOR, ISBN)
+        assertThat(book1 == book2).isTrue()
+    }
+}
