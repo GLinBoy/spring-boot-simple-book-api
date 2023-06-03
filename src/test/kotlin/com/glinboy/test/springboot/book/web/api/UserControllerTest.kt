cc58fe6136a39d1a7416a875de6fa06b10a391bb
GLinBoy <hojjatabedie@gmail.com>
2023-06-03 15:15:09 +0200

    📦 NEW: Added test class for UserController
    #59

---
+++ src/test/kotlin/com/glinboy/test/springboot/book/web/api/UserControllerTest.kt
@@ -0,0 +1,26 @@
+package com.glinboy.test.springboot.book.web.api
+
+import org.junit.jupiter.api.Test
+
+class UserControllerTest {
+
+    @Test
+    fun getUsers() {
+    }
+
+    @Test
+    fun getUser() {
+    }
+
+    @Test
+    fun addUser() {
+    }
+
+    @Test
+    fun deleteUser() {
+    }
+
+    @Test
+    fun updateUser() {
+    }
+}
