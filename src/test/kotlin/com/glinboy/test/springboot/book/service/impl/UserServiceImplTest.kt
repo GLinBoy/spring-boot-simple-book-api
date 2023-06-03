37a0385687b75e09be175c4d60cfa996cb89e149
GLinBoy <hojjatabedie@gmail.com>
2023-06-03 15:14:22 +0200

    📦 NEW: Added test class for UserServiceImpl
    #58

---
+++ src/test/kotlin/com/glinboy/test/springboot/book/service/impl/UserServiceImplTest.kt
@@ -0,0 +1,26 @@
+package com.glinboy.test.springboot.book.service.impl
+
+import org.junit.jupiter.api.Test
+
+class UserServiceImplTest {
+
+    @Test
+    fun register() {
+    }
+
+    @Test
+    fun getUsers() {
+    }
+
+    @Test
+    fun getUser() {
+    }
+
+    @Test
+    fun saveUser() {
+    }
+
+    @Test
+    fun deleteUser() {
+    }
+}
