fda34c3f5f4f12d78cfd5671e83405127f3a45ed
GLinBoy <hojjatabedie@gmail.com>
2023-12-14 20:32:30 +0100

    👌 IMPROVE: Upgraded Java to 21
    resolved #79

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 group = "com.glinboy.test"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_20
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 val springDocVersion = "2.1.0"
 
@@ -37,7 +37,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
