6a3ea94d432b80ad1d422476b02ed818b2d10dc7
GLinBoy <hojjatabedie@gmail.com>
2024-01-19 20:42:57 +0100

    👌 IMPROVE: Updated book service impl. methods to return BookDTO

--- src/main/kotlin/com/glinboy/test/springboot/book/service/impl/BookServiceImpl.kt
+++ src/main/kotlin/com/glinboy/test/springboot/book/service/impl/BookServiceImpl.kt
@@ -3,6 +3,7 @@ package com.glinboy.test.springboot.book.service.impl
 import com.glinboy.test.springboot.book.entity.Book
 import com.glinboy.test.springboot.book.repository.BookRepository
 import com.glinboy.test.springboot.book.service.BookServiceApi
+import com.glinboy.test.springboot.book.service.dto.BookDTO
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -14,7 +15,13 @@ class BookServiceImpl(val bookRepository: BookRepository): BookServiceApi {
         .findAll(pageable)
         .map { BookDTO(it.id, it.title, it.author, it.isbn) }
 
-    override fun getBook(id: Long): Optional<Book> = bookRepository.findById(id)
-    override fun saveBook(book: Book): Book = bookRepository.save(book)
+    override fun getBook(id: Long): Optional<BookDTO> = bookRepository
+        .findById(id)
+        .map { BookDTO(it.id, it.title, it.author, it.isbn) }
+    override fun saveBook(bookDTO: BookDTO): BookDTO {
+        var book = Book(bookDTO.id, bookDTO.title, bookDTO.author, bookDTO.isbn)
+        book = bookRepository.save(book)
+        return BookDTO(book.id, book.title, book.author, book.isbn)
+    }
     override fun deleteBook(id: Long) = bookRepository.deleteById(id)
 }
