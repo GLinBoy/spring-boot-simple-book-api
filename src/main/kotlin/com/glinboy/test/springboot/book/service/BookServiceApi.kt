9ffcaf27575e85e347255b8d8fe416d2234444b7
GLinBoy <hojjatabedie@gmail.com>
2024-01-19 20:36:47 +0100

    👌 IMPROVE: Updated BookServiceApi to return BookDTO

--- src/main/kotlin/com/glinboy/test/springboot/book/service/BookServiceApi.kt
+++ src/main/kotlin/com/glinboy/test/springboot/book/service/BookServiceApi.kt
@@ -1,13 +1,14 @@
 package com.glinboy.test.springboot.book.service
 
 import com.glinboy.test.springboot.book.entity.Book
+import com.glinboy.test.springboot.book.service.dto.BookDTO
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import java.util.*
 
 interface BookServiceApi {
-    fun getBooks(pageable: Pageable): Page<Book>
-    fun getBook(id: Long): Optional<Book>
-    fun saveBook(book: Book): Book
+    fun getBooks(pageable: Pageable): Page<BookDTO>
+    fun getBook(id: Long): Optional<BookDTO>
+    fun saveBook(book: Book): BookDTO
     fun deleteBook(id: Long)
 }
